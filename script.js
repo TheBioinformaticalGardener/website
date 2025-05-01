a9a1cd6a96055bd8a930aca414eda198e6caed5d
TheBioinformaticalGardener <167814751+TheBioinformaticalGardener@users.noreply.github.com>
2025-05-01 15:36:26 +0200

    Some major improvements regarding responsiveness

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const openNavButton = document.getElementById("open-nav-button");
 const navbar = document.getElementById("navbar");
+const main_content = document.getElementById("main-content");
 
 const media = window.matchMedia("(width < 700px)");
 
@@ -18,12 +19,14 @@ function openNav() {
   navbar.classList.add("show");
   openNavButton.setAttribute("aria-expanded", "true");
   navbar.removeAttribute("inert");
+  main_content.setAttribute("inert", "");
 }
 
 function closeNav() {
   navbar.classList.remove("show");
   openNavButton.setAttribute("aria-expanded", "false");
   navbar.setAttribute("inert", "");
+  main_content.removeAttribute("inert");
 }
 
 updateNav(media);
